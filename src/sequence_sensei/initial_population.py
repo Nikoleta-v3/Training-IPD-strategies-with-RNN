21268b11447ad2fb7297dfd0cbceb1c3e037a203
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-07-20 19:36:07 +0100

    fix to initial population
    Uses population size now

--- src/sequence_sensei/initial_population.py
+++ src/sequence_sensei/initial_population.py
@@ -1,7 +1,7 @@
 import numpy as np
 
 def get_initial_population(size_of_population, sequence_length=200):
-    cuts = np.linspace(1, sequence_length, 20, dtype = int)
+    cuts = np.linspace(1, sequence_length, size_of_population, dtype = int)
     sequences = []
     for cut in cuts:
         sequences.append([1 for _ in range(cut)] + [0 for _ in range(sequence_length - cut)])
