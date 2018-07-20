8e3c0de17793b867c7de7328de7e046e78f9adad
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-07-20 23:54:41 +0100

    implement mutation and crossover

---
+++ src/sequence_sensei/genetic_algorithm.py
@@ -0,0 +1,12 @@
+import random
+
+def crossover(sequence_one, sequence_two):
+    sequence_length = len(sequence_one)
+    crossover_point = random.randint(0, sequence_length)
+
+    return sequence_one[:crossover_point] + sequence_two[crossover_point:]
+
+def mutation(gene, mutation_probability):
+    if random.random() < mutation_probability:
+        return abs(gene - 1)
+    return gene
---
+++ tests/test_genetic_algorithm.py
@@ -0,0 +1,19 @@
+import sequence_sensei as ss
+import axelrod as axl
+import random
+
+def test_crossover():
+    sequence_one = [random.randint(0, 2) for _ in range(10)]
+    sequence_two = [random.randint(0, 2) for _ in range(10)]
+
+    axl.seed(0)
+    kid = ss.crossover(sequence_one, sequence_two)
+    assert kid == sequence_one[:6]+ sequence_two[6:]
+
+def test_mutation():
+    mutation_probability = 0.3
+    sequence = [1, 0, 1, 0, 1, 1, 0, 0]
+
+    axl.seed(1)
+    mutated_sequence = [ss.mutation(gene, mutation_probability) for gene in sequence]
+    assert mutated_sequence == [0] + sequence[1:3] + [1] + sequence[4:]
