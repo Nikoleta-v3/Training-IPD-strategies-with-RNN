3b3901ba4a7ca0f9230faecad1f55e8cfdb33d09
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2018-07-23 11:10:25 +0100

    finish off evolve function
    Need to search tqdm and make some nice printing to keep track of the
    generations

--- src/sequence_sensei/__init__.py
+++ src/sequence_sensei/__init__.py
@@ -1,3 +1,3 @@
 from .initial_population import get_initial_population
 from .fitness import get_sequence_str, get_fitness_of_individual, get_fitness_of_population
-from .genetic_algorithm import crossover, mutation
+from .genetic_algorithm import crossover, mutation, evolve
--- src/sequence_sensei/genetic_algorithm.py
+++ src/sequence_sensei/genetic_algorithm.py
@@ -2,6 +2,7 @@
 A file which contains code to perform a genetic algorithm on binary sequences.
 """
 import csv
+import os
 import random
 import sequence_sensei as ss
 
@@ -16,40 +17,56 @@ def mutation(gene, mutation_probability):
         return abs(gene - 1)
     return gene
 
+def subset_population(population, indices):
+    subset = []
+    for i in indices:
+        subset.append(population[i])
+    return subset
+
 def evolve(opponent, number_of_generations, bottleneck, mutation_probability,
-           sequence_length, size_of_population, seed, index, num_process=1):
+           sequence_length, size_of_population, seed, num_process=1):
 
-    headers = ['generation', 'opponent', 'seed', 'indes']
-    headers += ['gene_{}'.format(i) for i in range(sequence_length)] + ['score']
+    headers = ['generation', 'index', 'score']
+    headers += ['gene_{}'.format(i) for i in range(sequence_length)]
 
+    print('|Initialising population|')
     generation = 0
     population = ss.get_initial_population(size_of_population=size_of_population,
                                            sequence_length=sequence_length)
-    score = ss.get_fitness_of_population(population=population, opponent=opponent,
+    scores = ss.get_fitness_of_population(population=population, opponent=opponent,
                                          seed=seed, num_process=num_process)
-    # combine scores with initial population on index
-    results = zip()
 
-    # write file
-    # make folder {}_{}.format(opponent.name, seed)
-    with open("main.csv", "a") as data_file:
+    results = [[generation, *scores[i], *population[i]] for i in range(size_of_population * 2)]
+    results.sort(key=lambda tup:tup[1], reverse=True)
+
+    path = 'raw_data/{}_{}'.format(opponent.name, seed)
+    os.mkdir(path)
+    with open("{}/main.csv".format(path), "a") as data_file:
         data_writer = csv.writer(data_file)
         data_writer.writerow(headers)
-        data_writer.writerow(results)
+        for row in results:
+            data_writer.writerow(row)
+        print('|Finish Generation 0|')
 
-    while generation < number_of_generations:
-        indices_to_keep = [i for (i, s) in score[0: bottleneck]]
-        population = population[indices_to_keep] # this is going to break
+        while generation < number_of_generations:
+            generation += 1
+            indices_to_keep = [results[i][1] for i in range(bottleneck)]
+            population = subset_population(population, indices_to_keep)
 
-        while len(population) < 2 * size_of_population:
-            i, j  = [random.randint(0, bottleneck) for _ in range(2)]
-            new_individual = crossover(population[i], population[j])
-            new_individual = [mutation(gene, mutation_probability) for gene in new_individual]
-            population.append(new_individual)
-        
-        score = ss.get_fitness_of_population(population=population, opponent=opponent,
-                                             seed=seed, num_process=num_process)
-        # combine scores with initial population on index
-        results = zip()
-        data_writer.writerow(results)
-        generation += 1
+            while len(population) < 2 * size_of_population:
+                i, j  = [random.randint(0, bottleneck) for _ in range(2)]
+                new_individual = crossover(population[i], population[j])
+                new_individual = [mutation(gene, mutation_probability) for gene in new_individual]
+                population.append(new_individual)
+
+            scores = ss.get_fitness_of_population(population=population,
+                                                  opponent=opponent, seed=seed,
+                                                  num_process=num_process)
+
+            results = [[generation, *scores[i], *population[i]] for i in range(size_of_population * 2)]
+            results.sort(key=lambda tup:tup[1], reverse=True)
+            for row in results:
+                data_writer.writerow(row)
+            print('|Finish Generation {}|'.format(generation))
+            
+# TODO: List from indices in list
