c2bb91227cc17d5cd1bdf399d7135ac61c200cfa
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2020-02-13 17:36:08 +0000

    add function to read in trained models from hawk
    Also runs black

--- player_class.py
+++ player_class.py
@@ -2,8 +2,11 @@ import numpy as np
 
 import axelrod as axl
 from axelrod.random_ import random_choice
+from keras.layers import LSTM, Dense, Dropout
+from keras.models import Sequential
 
 C, D = axl.Action.C, axl.Action.D
+
 
 
 class LSTMPlayer(axl.Player):
@@ -30,7 +33,8 @@ class LSTMPlayer(axl.Player):
 
         history = [action.value for action in opponent.history]
         prediction = float(
-            self.model.predict(self.reshape_history_function(history))[0][-1])
+            self.model.predict(self.reshape_history_function(history))[0][-1]
+        )
 
         return axl.Action(round(prediction))
 
@@ -38,5 +42,32 @@ class LSTMPlayer(axl.Player):
         return self.name
 
 
-def reshape_history_simple_model(history):
+def reshape_history_lstm_model(history):
     return np.array(history).reshape(1, len(history), 1)
+
+
+def read_model_lstm(filename, num_hidden_cells=100, drop_out_rate=0.2):
+    model = Sequential()
+    model.add(
+        LSTM(num_hidden_cells, return_sequences=True, input_shape=(None, 1))
+    )
+    model.add(Dropout(rate=drop_out_rate))
+    model.add(Dense(1, activation="sigmoid"))
+    model.load_weights(filename)
+
+    return model
+
+
+def read_model_lstm_unknown_length(
+    filename, num_hidden_cells=100, drop_out_rate=0.2
+):
+    model = Sequential()
+    model.add(
+        LSTM(num_hidden_cells, return_sequences=True, input_shape=(None, 1))
+    )
+    model.add(LSTM(num_hidden_cells))
+    model.add(Dropout(rate=drop_out_rate))
+    model.add((Dense(1, activation="sigmoid")))
+    model.load_weights(filename)
+
+    return model
