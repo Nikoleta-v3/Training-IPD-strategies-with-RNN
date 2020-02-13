a14cc36abd1347a7ec60c3181848f052a09e7256
Nikoleta Glynatsi <glynatsine@cardiff.ac.uk>
2020-02-13 17:21:13 +0000

    copying down results from hawk

---
+++ copy_results.sh
@@ -0,0 +1,7 @@
+scp -r hawk:/home/c.c1569433/Training-IPD-strategies-with-RNN/output_lstm hawk_output
+scp -r hawk:/home/c.c1569433/Training-IPD-strategies-with-RNN/output_lstm_top_twenty hawk_output
+scp -r hawk:/home/c.c1569433/Training-IPD-strategies-with-RNN/output_lstm_model_basic hawk_output
+
+scp -r hawk:/home/c.c1569433/Training-IPD-strategies-with-RNN/output_lstm_unknown_model hawk_output
+scp -r hawk:/home/c.c1569433/Training-IPD-strategies-with-RNN/output_lstm_unknown_model_top_twenty hawk_output
+scp -r hawk:/home/c.c1569433/Training-IPD-strategies-with-RNN/output_lstm_unknown_model_basic hawk_output
